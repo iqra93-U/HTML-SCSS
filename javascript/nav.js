5b36646b5c32f7db9a1f862120a9bc4495845047
Iqra Tariq <tariqn.iqra@gmail.com>
2021-04-29 18:23:09 +0200

    modification in files

--- javascript/nav.js
+++
@@ -1,8 +0,0 @@
-const toggleMenu = document.querySelector('.main-nav__toggler');
-const menu = document.querySelector('.main-nav__list');
-
-toggleMenu.addEventListener('click', function() {
-  const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
-  toggleMenu.setAttribute('aria-expanded', !open);
-  menu.hidden = !menu.hidden;
-});
