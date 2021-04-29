966cabc36fa615b89938463eb79176a525e72273
Iqra Tariq <tariqn.iqra@gmail.com>
2021-04-29 19:24:50 +0200

    addition of top button

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,24 @@ toggleMenu.addEventListener('click', function() {
   toggleMenu.setAttribute('aria-expanded', !open);
   menu.hidden = !menu.hidden;
 });
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
