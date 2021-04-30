057087a3fa93ec20e173c435ad28f8600bf30c8f
Iqra Tariq <tariqn.iqra@gmail.com>
2021-04-30 13:51:18 +0200

    modification for top button

--- javascript/script.js
+++ javascript/script.js
@@ -9,14 +9,14 @@ toggleMenu.addEventListener('click', function() {
 
 
 //Get the button
-var mybutton = document.getElementById("myBtn");
+var mybutton = document.querySelector(".myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    mybutton.style.display = "initial";
   } else {
     mybutton.style.display = "none";
   }
